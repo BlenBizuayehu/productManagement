2dc69486bbeab8ce44964b517c8e7b9a75d552c7
blen bizuayehu <blenaqua57js@gmail.com>
2024-12-04 23:32:57 +0300

    create operation done

--- productController.js
+++ productController.js
@@ -1,11 +1,19 @@
 const express=require("express");
+const Product=require("./productModel")
 exports.GetAllProduct=async(req,res)=>{
     const productList=["chair","table","skirt","shoes","broom"];
     res.send(productList);
 }
 exports.AddProduct=async(req,res)=>{
-    const data=req.body;
-    res.send(data);
-    console.log(data.ProductQuantity);
+    try {
+        const{name, price}=req.body;
+        const product=await Product.create({name,price});
+        res.send(product);
+    } catch (error) {
+        
+    }
+    // const data=req.body;
+    // res.send(data);
+    // console.log(data.ProductQuantity);
 }
 
--- server.js
+++ server.js
@@ -1,16 +1,18 @@
+require("dotenv").config();
 const express=require("express");
+const connectDB = require("./config/DB");
+
+const cors=require("cors");
 const app=express();
 app.use(express.json());
-// app.use("/",(req,res)=>{
+app.use(cors());
 
-//     res.send("this is the landing page")
-//     }
-// );
+connectDB();
 app.use("/product",require("./productRoute"));
 
 app.use("/order",require("./orderRoute"));
 
-app.listen(5001,()=>{
-    console.log("Server is up on port 5001");
+app.listen(5003,()=>{
+    console.log("Server is up on port 5003");
 });
 
